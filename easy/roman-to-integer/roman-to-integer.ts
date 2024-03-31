090ea5ef0f4a3d238ad481f3445551749f7f902c
Florian Hamel <fhamel.freelance@gmail.com>
2024-03-31 16:05:07 +0200

    🧪 easy: roman to integer

--- easy/roman-to-integer/roman-to-integer.ts
+++ easy/roman-to-integer/roman-to-integer.ts
@@ -1,13 +1,20 @@
-export function romanToInt(s: string): number {
-  const map: Map<string, number> = new Map<string, number>([
-    ['I', 1],
-    ['V', 5],
-    ['X', 10],
-    ['L', 50],
-    ['C', 100],
-    ['D', 500],
-    ['M', 1000],
-  ]);
+const map: Map<string, number> = new Map<string, number>([
+  ['I', 1],
+  ['V', 5],
+  ['X', 10],
+  ['L', 50],
+  ['C', 100],
+  ['D', 500],
+  ['M', 1000],
+]);
 
-  [...s].forEach([])
+export function romanToInt(s: string): number {
+  let output = 0;
+  for (let i = 0; i < s.length; ++i) {
+    const curr = map.get(s.charAt(i))
+    const next = map.get(s.charAt(i + 1));
+    if (i < s.length - 1 && curr < next) output -= curr;
+    else output += curr;
+  }
+  return output;
 }
